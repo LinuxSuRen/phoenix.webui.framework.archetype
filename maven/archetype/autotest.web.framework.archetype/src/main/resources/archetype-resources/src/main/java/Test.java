fd2a091fd06eca7f15b740913d4210d4048e4630
suren <suren@0b0a54f1-932d-4d7b-9bbf-d20aa1ac5a98>
2016-07-28 00:01:18 +0000

    增加测试的示例

--- maven/archetype/autotest.web.framework.archetype/src/main/resources/archetype-resources/src/main/java/Test.java
+++ maven/archetype/autotest.web.framework.archetype/src/main/resources/archetype-resources/src/main/java/Test.java
@@ -7,6 +7,7 @@ import ${package}.page.SuRenPiHomePage;
 import org.suren.autotest.web.framework.settings.SettingUtil;
 
 /**
+ * 测试示例
  * @author suren
  * @date Jul 23, 2016 4:48:39 PM
  */
@@ -18,19 +19,30 @@ public class Test
 	 */
 	public static void main(String[] args) throws Exception
 	{
-		SettingUtil util = new SettingUtil("${package}.page"); //这里的参数是Page类所在的包（package）
-		util.readFromClassPath("surenpi_page.xml"); //从resources目录下读取配置文件
-		util.initData();
-		
-		SuRenPiHomePage homePage = util.getPage(SuRenPiHomePage.class);
-		homePage.open(); //打开浏览器地址
-		homePage.getSearchText().fillValue().performEnter();
+		SettingUtil util = new SettingUtil("org.suren.auotest.autotest.web.framework.test.page"); //这里的参数是Page类所在的包（package
+		try
+		{
+			util.readFromClassPath("surenpi_page.xml"); //从resources目录下读取配置文件
+			util.initData(); //从数据源中初始化数据
+			
+			SuRenPiHomePage homePage = util.getPage(SuRenPiHomePage.class);
+			homePage.open(); //打开浏览器地址
+			homePage.getSearchText().fillValue().performEnter(); //输入搜索关键字并按回车确认
 
-		homePage.getCloseBut().click(); //关闭右下角公告
-		
-		Thread.sleep(9000);
-		
-		homePage.close();
+			homePage.getCloseBut().click(); //关闭右下角公告
+			
+			homePage.getChargeText().fillValue(); //填入淘宝手机充值号码
+			
+			homePage.getCheckBoxGroup().selectByText("50"); //设置要充值的金额
+			
+			homePage.getChargeBut().click(); //点击充值确认按钮
+			
+			Thread.sleep(9000); //这里为了能让用户更方便地看到自动化测试效果
+		}
+		finally
+		{
+			util.close(); //在程序最终结束后应该调用close方法来关闭浏览器引擎，不然可能发生资源泄漏的问题
+		}
 	}
 
 }
