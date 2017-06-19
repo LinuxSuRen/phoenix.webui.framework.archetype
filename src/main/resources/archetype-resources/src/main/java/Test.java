624fb82c4e8baffb6aacac1cb53b9e711231674e
surenpi.com <LinuxSuRen@gmail.com>
2017-06-19 18:58:57 +0800

    modify package configration

--- src/main/resources/archetype-resources/src/main/java/Test.java
+++ src/main/resources/archetype-resources/src/main/java/Test.java
@@ -1,3 +1,21 @@
+/*
+ *
+ *  * Copyright 2002-2007 the original author or authors.
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
 /**
  * http://surenpi.com
  */
@@ -17,7 +35,7 @@ import org.suren.autotest.web.framework.util.ThreadUtil;
  * @date Jul 23, 2016 4:48:39 PM
  */
 @AutoApplication(name = "Phoenix平台自动化测试",
-		scanBasePackages = {"com.surenpi.autotest.demo",
+		scanBasePackages = {"${package}",
 				"com.surenpi.autotest.report"
 		})
 public class Test
